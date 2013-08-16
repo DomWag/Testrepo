312e02bd6d09c42634fec8e085a3e27bfa1f02cf
Dominik Wagner <dominik-wagner@live.de>
2013-08-16 20:22:41 +0200

    Testjava

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Dominik Wagner
+ * Date: 16.08.13
+ * Time: 20:14
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
